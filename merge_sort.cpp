7ad9e265b905f8e79bd2c97b158b0ace3e780a6a
Charles <likekite1998@gmail.com>
2017-02-25 10:37:57 +0800

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int A[] = {6,5,3,1,8,7,2,4},temp[8];
-	int n=8, l1 , l2, h1, h2, k, i, j, m=0;
+int main() {	
+	int arr[] = {6, 5, 3, 1, 8, 7, 2, 4}, temp[100];
+	int n = 8, i, j, k, l1, l2, h1, h2, size, p = 0;
 	
-	for (int k=0;k<n;k++)
-		cout << A[k] << " ";
-	cout << endl;
-	
-	for (int size = 1; size < n; size = size * 2){
+	cout << "Array is: \n";
+	for(int k = 0; k < n; k++){
+		cout<<arr[k]<<" ";
+ 	}
+ 	cout<<endl<<endl;
+ 	for(size = 1; size < n; size = size * 2) {
 		l1 = 0;
 		k = 0;
-		m++;
-		while (l1 + size < n){
-			h1 = l1 + size-1;
+		p++;
+		while(l1 + size < n) {
+			h1 = l1 + size - 1;
 			l2 = h1 + 1;
-			h2 = l2 + size-1;
-			if (h2 >= n)
-				h2 = n-1;
-			i= l1;
-			j= l2;
-			while (i <= h1 && j <= h2){
-				if(A[i] <= A[j])
-					temp[k++] = A[i++];
+			h2 = l2 + size - 1;
+			if(h2 >= n)
+				h2 = n - 1;
+			i = l1;
+			j = l2;
+			while(i <= h1 && j <= h2) {
+				if(arr[i] <= arr[j])
+					temp[k++] = arr[i++];
 				else
-					temp[k++] = A[j++];	
+					temp[k++] = arr[j++];
 			}
-			while (i <= h1)
-				temp[k++] = A[i++];
-			while (j <= h2)
-				temp[k++] = A[j++];
-			l1 = h2+1;			
+			while(i <= h1)
+					temp[k++] = arr[i++];
+			while(j <= h2)
+					temp[k++] = arr[j++];
+			l1 = h2 + 1;
 		}
-		for (i = l1; k < n; i++)
-			temp[k++] = A[i];
-		for(i = 0; i < n; i++)
-			A[i] = temp[i];
-		for (int k = 0; k < n; k++)
-			cout << A[k] << " ";
-		cout << endl;		
+		for(i = l1; k < n; i++)
+			temp[k++] = arr[i];
+		for(i=0; i < n; i++)
+			arr[i] = temp[i];
+			cout << "Pass " << p << ": \n";
+		for(int k = 0; k < n; k++)
+		cout << arr[k] << " ";
+		cout << endl;
 	}
 }
